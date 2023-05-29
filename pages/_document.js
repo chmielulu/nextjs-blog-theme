d5fcb7cb72252b650929ed91e6bd8e7a7347b8f8
Jakub Maciej Chmielewski <53061612+chmielulu@users.noreply.github.com>
2023-05-29 12:22:22 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
-        <Head />
+        <Head>
+          <script defer data-domain="heartfelt-custard-8b4076.netlify.app" src="https://plausible.io/js/script.js"></script>
+        </Head>
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
